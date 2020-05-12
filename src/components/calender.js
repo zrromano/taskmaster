e245ee51b334a50fa9d939432598beaecba656a7
Matt Frankmore <m.frankmore@outlook.com>
2020-05-12 12:51:24 -0600

    Fixed issue with page not displaying properly

--- src/components/calender.js
+++ src/components/calender.js
@@ -1,7 +1,67 @@
 import React from "react";
 
 const Calender = () => {
-  return <h1>display a calender with all tasks that have due dates</h1>;
+  
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return (
+    <div class="calendar" id="calendar">
+      <div class="calendar-btn month-btn" onclick="$('#months').toggle('fast')">
+          <span id="curMonth"></span>
+          <div id="months" class="months dropdown"></div>
+      </div>
+
+      <div class="calendar-btn year-btn" onclick="$('#years').toggle('fast')">
+          <span id="curYear"></span>
+          <div id="years" class="years dropdown"></div>
+      </div>
+
+      <div class="clear"></div>
+
+      <div class="calendar-dates">
+          <div class="days">
+              <div class="day label">SUN</div>
+              <div class="day label">MON</div>
+              <div class="day label">TUE</div>
+              <div class="day label">WED</div>
+              <div class="day label">THUR</div>
+              <div class="day label">FRI</div>
+              <div class="day label">SAT</div>
+
+              <div class="clear"></div>
+          </div>
+
+          <div id="calendarDays" class="days">
+          </div>
+      </div>
+    </div>
+);
 };
+
+
+
 
 export default Calender;
