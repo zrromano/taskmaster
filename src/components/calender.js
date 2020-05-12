43775967e93e798f7a888e3cd50afd9d679b0988
Matt Frankmore <m.frankmore@outlook.com>
2020-05-12 16:35:10 -0600

    Fixed dropdown

--- src/components/calender.js
+++ src/components/calender.js
@@ -1,23 +1,24 @@
 import React from "react";
-import "../calender.css"
+import "../calender.css";
+import Select from "./common/select"
 
 const Calender = () => {
-  
 
   let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
+    {_id:1 , name: "January"},
+    {_id:2 , name: "February"},
+    {_id:3 , name: "March"},
+    {_id:4 , name: "April"},
+    {_id:5 , name: "May"},
+    {_id:6 , name: "June"},
+    {_id:7 , name: "July"},
+    {_id:8 , name: "August"},
+    {_id:9 , name: "September"},
+    {_id:10 , name: "October"},
+    {_id:11 , name: "November"},
+    {_id:12 , name: "December"}
+  ]
+
   let days = [
     "Sunday",
     "Monday",
@@ -30,14 +31,8 @@ const Calender = () => {
 
   return (
     <div class="calendar" id="calendar">
-      <div class="calendar-btn month-btn" onclick="$('#months').toggle('fast')">
-          <span id="curMonth"></span>
-          <div id="months" class="months dropdown"></div>
-      </div>
-
-      <div class="calendar-btn year-btn" onclick="$('#years').toggle('fast')">
-          <span id="curYear">testClnBtn</span>
-          <div id="years" class="years dropdown"></div>
+      <div class="calendar-btn month-btn">
+         <Select options= {months}/>
       </div>
 
       <div class="clear"></div>
